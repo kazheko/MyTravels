c1158b0f414ca7dcfe75f2e62c04d8cfe9da2a62
ekazheko <kazheko@xaker.ru>
2022-07-06 12:48:36 +0300

    generate password; add security group

--- MyTravels/Tools/Terraform/stage/data-storage/postgresql/main.tf
+++ MyTravels/Tools/Terraform/stage/data-storage/postgresql/main.tf
@@ -13,6 +13,15 @@ provider "aws" {
   region = "us-east-2"
 }
 
+provider "random" {
+}
+
+resource "random_password" "password" {
+  length = 20
+  special = false
+  override_special = "_%@"
+}
+
 #creates a database in RDS
 resource "aws_db_instance" "db" {
   identifier_prefix = "stage"
@@ -20,7 +29,20 @@ resource "aws_db_instance" "db" {
   allocated_storage = 5
   instance_class = "db.t3.micro"
   db_name = "mytravels"
-  skip_final_snapshot = true 
+  skip_final_snapshot = true
+  publicly_accessible  = true
   username = var.db_username
-  password = var.db_password
+  password = random_password.password.result
+  vpc_security_group_ids = [aws_security_group.db_asg.id]
+}
+
+resource "aws_security_group" "db_asg" {
+  name = "stage_db_asg"
+  description = "Allow all inbound for Postgres"
+  ingress {
+    from_port = 5432
+    to_port = 5432
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
--- MyTravels/Tools/Terraform/stage/data-storage/postgresql/variables.tf
+++ MyTravels/Tools/Terraform/stage/data-storage/postgresql/variables.tf
@@ -1,9 +1,5 @@
 variable "db_username" {
  description = "The username for the database"
  type = string
-}
-
-variable "db_password" {
- description = "The password for the database"
- type = string
+ default = "postgres"
 }
