07a628a3206dd2a8db67119b4044ff262d43d2d2
ekazheko <kazheko@xaker.ru>
2022-07-08 19:06:09 +0300

    add API for health check

---
+++ MyTravels/MyTravels.WebApi/Configurations/HealthCheckDIExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace MyTravels.WebApi.Configurations
+{
+    public static class HealthCheckDIExtensions
+    {
+        public static IServiceCollection AddLiveHealthChecks(this IServiceCollection service, params string[] tags)
+        {
+            service.AddHealthChecks()
+                .AddCheck("LiveHealthCheck", () => HealthCheckResult.Healthy(), tags);
+
+            return service;
+        }
+    }
+}
--- MyTravels/MyTravels.WebApi/Program.cs
+++ MyTravels/MyTravels.WebApi/Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using MyTravels.Persistence;
 using MyTravels.WebApi.Configurations;
+
+const string healthCheckTagLive = "live";
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.ClearProviders();
 builder.Logging.AddSerilogLogging();
+
+builder.Services.AddLiveHealthChecks(healthCheckTagLive);
 
 builder.Services.AddPooledDbContextFactory<ApplicationContext>(x =>
     x.UseNpgsqlProvider(builder.Configuration.GetDefaultConnection()));
@@ -14,5 +19,7 @@ builder.Services.AddGraphQL();
 var app = builder.Build();
 
 app.MapGraphQL();
+
+app.MapHealthChecks("/health/live", new HealthCheckOptions { Predicate = x => x.Tags.Contains(healthCheckTagLive) });
 
 app.Run();
